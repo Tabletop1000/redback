bc12db5cd4c98e35e2a59a49b633a0f602e19e91
Jadon Dutra <jadondutra@gmail.com>
2023-08-22 21:04:59 +1000

    adds encoder setup function to redbacklib

---
+++ main/redbacklib.c
@@ -0,0 +1,99 @@
+#include "redbacklib.h"
+
+rb_err_t rb_fcreate_linear(rb_fparam_linear_t *c)
+{
+    if ((c->m < -1) || (c->m > 1))
+        return rb_OUT_OF_RANGE;
+    c->t1 = abs( (c->y1 - c->y0) / c->m) + c->t0;
+    c->m = (c->y1 >= c->y0) ? c->m : c->m*-1;
+    return rb_OK;
+}
+
+rb_err_t rb_feval_linear(const rb_fparam_linear_t *c, const time_t t, float *y) {
+
+    if (t < c->t0){
+        return rb_OUT_OF_RANGE;
+    }
+    if (t >= c->t1){
+        *y = c->y1;
+        return rb_COMPLETE;
+    }
+    *y = c->m*(t-c->t0) + c->y0;
+    return rb_OK;
+}
+
+int32_t rb_map_float_to_int(const int y0,const int y1,const float x0,const float x1, float x){
+    float m = (y1 - y0)/(x1 - x0);
+    return (int32_t)(m*x);
+}
+
+void set_motor_direction(bdc_motor_handle_t motor,float current_value)
+{
+    static rb_direction_t dir = DIRECTION_FORWARD;
+
+    if((current_value < 0) && (dir != DIRECTION_REVERSE)) {
+        ESP_ERROR_CHECK(bdc_motor_reverse(motor));
+        dir = DIRECTION_REVERSE;
+    }
+    else if((current_value >= 0) && (dir != DIRECTION_FORWARD)) {
+        ESP_ERROR_CHECK(bdc_motor_forward(motor));
+        dir = DIRECTION_FORWARD;
+    }
+}
+
+bdc_motor_handle_t new_motor(uint32_t pin_a, uint32_t pin_b)
+{
+    bdc_motor_handle_t motor = NULL; // Permenant memory
+    bdc_motor_config_t motor_conf = {
+        .pwm_freq_hz = MCPWM_FREQ_HZ,
+        .pwma_gpio_num = pin_a,
+        .pwmb_gpio_num = pin_b,
+    };
+    bdc_motor_mcpwm_config_t mcpwm_conf = {
+        .group_id = 0,
+        .resolution_hz = MCPWM_TIMER_RESOLUTION_HZ,
+    };
+    ESP_ERROR_CHECK(bdc_motor_new_mcpwm_device(&motor_conf, &mcpwm_conf, &motor));
+    return motor;
+}
+
+pcnt_unit_handle_t new_encoder(uint32_t pin_a, uint32_t pin_b, uint32_t ppr)
+{
+    pcnt_unit_handle_t encoder = NULL;
+    pcnt_unit_config_t encoder_conf = {
+        .flags.accum_count = true,
+        .high_limit = ppr,
+        .low_limit = 0
+    };
+    ESP_ERROR_CHECK(pcnt_new_unit(&encoder_conf, &encoder));
+    pcnt_glitch_filter_config_t fliter_conf = {
+        .max_glitch_ns = 1000,
+    };
+    ESP_ERROR_CHECK(pcnt_unit_set_glitch_filter(encoder,&fliter_conf));
+    pcnt_chan_config_t chan_a_config = {
+        .edge_gpio_num = pin_a,
+        .level_gpio_num = pin_b,
+    };
+    pcnt_chan_config_t chan_b_config = {
+        .edge_gpio_num = pin_b,
+        .level_gpio_num = pin_a,
+    };
+    
+    pcnt_channel_handle_t chan_a = NULL;
+    pcnt_channel_handle_t chan_b = NULL;
+
+    ESP_ERROR_CHECK(pcnt_new_channel(encoder,&chan_a_config,&chan_a));
+    ESP_ERROR_CHECK(pcnt_new_channel(encoder,&chan_b_config,&chan_b));
+
+    ESP_ERROR_CHECK(pcnt_channel_set_edge_action(chan_a, PCNT_CHANNEL_EDGE_ACTION_DECREASE, PCNT_CHANNEL_EDGE_ACTION_INCREASE));
+    ESP_ERROR_CHECK(pcnt_channel_set_level_action(chan_a, PCNT_CHANNEL_LEVEL_ACTION_KEEP, PCNT_CHANNEL_LEVEL_ACTION_INVERSE));
+
+    ESP_ERROR_CHECK(pcnt_channel_set_edge_action(chan_b, PCNT_CHANNEL_EDGE_ACTION_INCREASE, PCNT_CHANNEL_EDGE_ACTION_DECREASE));
+    ESP_ERROR_CHECK(pcnt_channel_set_level_action(chan_b, PCNT_CHANNEL_LEVEL_ACTION_KEEP, PCNT_CHANNEL_LEVEL_ACTION_INVERSE));
+
+    ESP_ERROR_CHECK(pcnt_unit_enable(encoder));
+    ESP_ERROR_CHECK(pcnt_unit_clear_count(encoder));
+    ESP_ERROR_CHECK(pcnt_unit_start(encoder));
+
+    return encoder;
+}
---
+++ main/redbacklib.h
@@ -0,0 +1,101 @@
+/**
+ * @file redbacklib.h
+ * @brief set of functions for motor control
+*/
+#ifndef REDBACK_LIB_H
+#define REDBACK_LIB_H
+
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "sdkconfig.h"
+#include "esp_log.h"
+#include "driver/mcpwm_prelude.h"
+#include "driver/pulse_cnt.h"
+#include "bdc_motor.h"
+
+#define MCPWM_FREQ_HZ 10000 // 10 kHz
+#define MCPWM_GPIO_A 32
+#define MCPWM_GPIO_B 33
+#define MCPWM_TIMER_RESOLUTION_HZ 10000000 // 10 MHz
+#define MCPWM_DUTY_TICK_MAX       (MCPWM_TIMER_RESOLUTION_HZ / MCPWM_FREQ_HZ) // maximum value we can set for the duty cycle, in ticks
+
+typedef enum{
+    rb_OK,
+    rb_FAIL,
+    rb_OUT_OF_RANGE,
+    rb_COMPLETE
+}rb_err_t;
+
+typedef enum{
+    IN_PROGRESS,
+    COMPLETE,
+    TIMEOUT
+}rb_action_t;
+
+typedef enum {
+    DIRECTION_FORWARD,
+    DIRECTION_REVERSE
+}rb_direction_t;
+
+// Linear function definition
+typedef struct{
+    float m;    // rate
+    float y0;   // current value
+    float y1;   // goal
+    time_t t0;  // start time
+    time_t t1;  // end time
+} rb_fparam_linear_t;
+
+/**
+ * @brief Create a linear (y = mx + c) function
+ * @param c function parameters
+ * @returns rb_OK if successful, otherwise an out of range error
+*/
+rb_err_t rb_fcreate_linear(rb_fparam_linear_t *c);
+
+/**
+ @brief performs a linear interpolation between two points at a specific time
+ @param c pointer to function parameters
+ @param y pointer to the resulting y value
+ @param t pointer to current time
+*/
+rb_err_t rb_feval_linear(const rb_fparam_linear_t *c, const time_t t, float *y);
+
+/** 
+ * @brief maps a floating point number to a signed 32 bit int
+ * @param y0 lowest value of new range
+ * @param y1 highest value of new range
+ * @param x0 lowest value of new range
+ * @param x1 highest value of old range
+ * @param x current x value to evaluate at
+ * @returns signed int
+*/
+int32_t rb_map_float_to_int(const int y0,const int y1,const float x0,const float x1, float x);
+
+/**
+ * @brief Changes motor direction based on sign of value demand
+ * @param motor ponter to bdc motor handle
+ * @param current_value the signed float value
+*/
+void set_motor_direction(bdc_motor_handle_t motor,float current_value);
+
+/**
+ * @brief Creates a motor instance
+ * @param pin_a GPIO A
+ * @param pin_b GPIO B
+ * @returns bdc motor handle
+*/
+bdc_motor_handle_t new_motor(uint32_t pin_a, uint32_t pin_b);
+
+/**
+ * @brief Creates an encoder instance
+ * @param pin_a GPIO A
+ * @param pin_b GPIO B
+ * @param ppr Encoder pulses per rotation
+ * @returns encodr handle
+*/
+pcnt_unit_handle_t new_encoder(uint32_t pin_a, uint32_t pin_b, uint32_t ppr);
+
+#endif
